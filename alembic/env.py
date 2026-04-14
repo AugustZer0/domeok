e34ce502899d9b6af8ae29691d24d9cbe26e04cf
AugustZer0 <153372959+AugustZer0@users.noreply.github.com>
2026-04-14 18:36:30 +0900

    feat: 예외처리 관련 설정(#5) (#6)
    * feat:유저 관련 모델 설계 및 스키마 정의(#3)
    
    * feat:예외처리 관련 설정(#5)

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,9 @@
 import asyncio
 import os
 import sys
+import pkgutil
+import importlib
+import domains
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
@@ -12,9 +15,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from src.core.config import settings
-from src.core.database import Base
-# from src.domains.user.models import User 나중에 들어가야함
+from core.config import settings
+from core.database import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,6 +29,9 @@ config.set_main_option("sqlalchemy.url", settings.POSTGRES_DATABASE_URL)
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+for loader, module_name, is_pkg in pkgutil.walk_packages(domains.__path__, domains.__name__ + "."):
+    importlib.import_module(module_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
